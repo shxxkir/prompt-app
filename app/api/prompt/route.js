300b995238f21d75549561c8fd89fcdf94a7c72b
shxxkir <128359891+shxxkir@users.noreply.github.com>
2025-02-10 10:01:10 +0530

    update

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,10 +5,6 @@ export const GET = async (request) => {
     try {
         await connectToDB();
 
-        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-        res.setHeader("Pragma", "no-cache");
-        res.setHeader("Expires", "0");
-        
         const prompts = await Prompt.find({}).populate('creator');
 
         return new Response(JSON.stringify(prompts), { status: 200 })
